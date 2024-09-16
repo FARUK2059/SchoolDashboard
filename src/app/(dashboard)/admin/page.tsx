f271aab17663d6dccfc31644c5de95b08fb2d003
FARUK2059 <omarfaruk34552@gmail.com>
2024-09-17 00:17:13 +0600

    dashboard setup Start

---
+++ src/app/(dashboard)/admin/page.tsx
@@ -0,0 +1,12 @@
+
+
+
+const page = () => {
+    return (
+        <div>
+            <p>This is Admin</p>
+        </div>
+    );
+};
+
+export default page;
---
+++ src/app/(dashboard)/student/page.tsx
@@ -0,0 +1,12 @@
+
+
+
+const page = () => {
+    return (
+        <div>
+            <p>This is student Components</p>
+        </div>
+    );
+};
+
+export default page;
