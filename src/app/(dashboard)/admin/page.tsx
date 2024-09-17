766272c5ed5e07d1254ce05be4b93be36eaaf6a7
FARUK2059 <omarfaruk34552@gmail.com>
2024-09-17 18:14:45 +0600

    Admin data bord design Complet

--- src/app/(dashboard)/admin/page.tsx
+++ src/app/(dashboard)/admin/page.tsx
@@ -1,10 +1,55 @@
+import UserCard from "@/components/UserCard";
 
 
 
 const page = () => {
     return (
         <div>
-            <p>This is Admin</p>
+            <div className="p-4 flex gap-4 flex-col md:flex-row">
+
+                {/* LEFT */}
+                <div className="w-full lg:w-2/3 flex flex-col gap-8">
+
+                    {/* USER CARDS */}
+                    <div className="flex gap-4 justify-between  flex-wrap">
+                        <div className="bg-fPurple rounded-2xl">
+                            <UserCard type="student" />
+                        </div>
+                        <div className="bg-fYellow rounded-2xl">
+                            <UserCard type="teacher" />
+                        </div>
+                        <div className="bg-fPurple rounded-2xl" >
+                            <UserCard type="parent" />
+                        </div>
+                        <div className="bg-fYellow rounded-2xl" >
+                            <UserCard type="staff" />
+                        </div>
+
+                    </div>
+                    
+                    {/* MIDDLE CHARTS */}
+                    <div className="flex gap-4 flex-col lg:flex-row">
+                        {/* COUNT CHART */}
+                        <div className="w-full lg:w-1/3 h-[450px]">
+                            {/* <CountChart /> */}
+                        </div>
+                        {/* ATTENDANCE CHART */}
+                        <div className="w-full lg:w-2/3 h-[450px]">
+                            {/* <AttendanceChart /> */}
+                        </div>
+                    </div>
+                    {/* BOTTOM CHART */}
+                    <div className="w-full h-[500px]">
+                        {/* <FinanceChart /> */}
+                    </div>
+                </div>
+
+                {/* RIGHT */}
+                <div className="w-full lg:w-1/3 flex flex-col gap-8">
+                    {/* <EventCalendar /> */}
+                    {/* <Announcements /> */}
+                </div>
+            </div>
         </div>
     );
 };
