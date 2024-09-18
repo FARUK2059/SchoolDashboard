75945a1b7c4aa8b5f5b3b1a647108590c9d563d5
FARUK2059 <omarfaruk34552@gmail.com>
2024-09-19 03:32:14 +0600

    Subject page Setup Complet

--- src/app/(dashboard)/list/subjects/page.tsx
+++ src/app/(dashboard)/list/subjects/page.tsx
@@ -1,38 +1,25 @@
 import Pagination from "@/components/Pagination";
 import Table from "@/components/Table";
 import TableSearch from "@/components/TableSearch";
-import { parentsData, role } from "@/lib/data";
+import { role, subjectsData } from "@/lib/data";
 import Image from "next/image";
 
 
-type Parent = {
+type Subject = {
     id: number;
     name: string;
-    email?: string;
-    students: string[];
-    phone: string;
-    address: string;
+    teachers: string[];
 };
 
 const columns = [
     {
-        header: "Info",
-        accessor: "info",
+        header: "Subject Name",
+        accessor: "name",
     },
     {
-        header: "Student Names",
-        accessor: "students",
+        header: "Teachers",
+        accessor: "teachers",
         className: "hidden md:table-cell",
-    },
-    {
-        header: "Phone",
-        accessor: "phone",
-        className: "hidden lg:table-cell",
-    },
-    {
-        header: "Address",
-        accessor: "address",
-        className: "hidden lg:table-cell",
     },
     {
         header: "Actions",
@@ -43,27 +30,20 @@ const columns = [
 
 const page = () => {
 
-    const renderRow = (item: Parent) => (
+    const renderRow = (item: Subject) => (
         <tr
             key={item.id}
             className="border-b border-gray-200 even:bg-slate-50 text-sm hover:bg-fPurpleLight"
         >
-            <td className="flex items-center gap-4 p-4">
-                <div className="flex flex-col">
-                    <h3 className="font-semibold">{item.name}</h3>
-                    <p className="text-xs text-gray-500">{item?.email}</p>
-                </div>
-            </td>
-            <td className="hidden md:table-cell">{item.students.join(",")}</td>
-            <td className="hidden md:table-cell">{item.phone}</td>
-            <td className="hidden md:table-cell">{item.address}</td>
+            <td className="flex items-center gap-4 p-4">{item.name}</td>
+            <td className="hidden md:table-cell">{item.teachers.join(",")}</td>
             <td>
                 <div className="flex items-center gap-2">
                     {role === "admin" && (
                         <>
-                            {/* <FormModal table="parent" type="update" data={item} /> */}
-                            {/* <FormModal table="parent" type="delete" id={item.id} /> */}
-                            <p>Modal Open</p>
+                            <p>Modal</p>
+                            {/* <FormModal table="subject" type="update" data={item} />
+                            <FormModal table="subject" type="delete" id={item.id} /> */}
                         </>
                     )}
                 </div>
@@ -71,12 +51,11 @@ const page = () => {
         </tr>
     );
 
-
     return (
         <div className="bg-white p-4 rounded-md flex-1 m-4 mt-0">
             {/* TOP */}
             <div className="flex items-center justify-between">
-                <h1 className="hidden md:block text-lg font-semibold">All Parents</h1>
+                <h1 className="hidden md:block text-lg font-semibold">All Subjects</h1>
                 <div className="flex flex-col md:flex-row items-center gap-4 w-full md:w-auto">
                     <TableSearch />
                     <div className="flex items-center gap-4 self-end">
@@ -86,15 +65,15 @@ const page = () => {
                         <button className="w-8 h-8 flex items-center justify-center rounded-full bg-fYellow">
                             <Image src="/sort.png" alt="" width={14} height={14} />
                         </button>
-                        {role === "admin" && (
-                            <p>Modal</p>
+                        {role === "admin" &&
                             // <FormModal table="teacher" type="create" />
-                        )}
+                            <p>modal</p>
+                        }
                     </div>
                 </div>
             </div>
             {/* LIST */}
-            <Table columns={columns} renderRow={renderRow} data={parentsData} />
+            <Table columns={columns} renderRow={renderRow} data={subjectsData} />
             {/* PAGINATION */}
             <Pagination />
         </div>
