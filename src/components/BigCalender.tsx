54157169bccf33d68d1f66610c8aa771a075eb25
FARUK2059 <omarfaruk34552@gmail.com>
2024-09-18 21:08:35 +0600

    Update 2 student big Calender

--- src/components/BigCalender.tsx
+++ src/components/BigCalender.tsx
@@ -1,11 +1,11 @@
 "use client";
 
-import { Calendar, momentLocalizer, View, Views } from "react-big-calendar";
 
-import "react-big-calendar/lib/css/react-big-calendar.css";
-import { useState } from "react";
-import moment from "moment";
 import { calendarEvents } from "@/lib/data";
+import moment from "moment";
+import { useState } from "react";
+import { Calendar, momentLocalizer, View, Views } from "react-big-calendar";
+import "react-big-calendar/lib/css/react-big-calendar.css";
 
 const localizer = momentLocalizer(moment);
 
@@ -19,18 +19,18 @@ const BigCalender = () => {
     };
 
     return (
-        <Calendar
-            localizer={localizer}
-            events={calendarEvents}
-            startAccessor="start"
-            endAccessor="end"
-            views={["work_week", "day"]}
-            view={view}
-            style={{ height: "98%" }}
-            onView={handleOnChangeView}
-            min={new Date(2025, 1, 0, 8, 0, 0)}
-            max={new Date(2025, 1, 0, 17, 0, 0)}
-        />
+            <Calendar
+                localizer={localizer}
+                events={calendarEvents}
+                startAccessor="start"
+                endAccessor="end"
+                views={["work_week", "day"]}
+                view={view}
+                style={{ height: "98%" }}
+                onView={handleOnChangeView}
+                min={new Date(2025, 1, 0, 8, 0, 0)}
+                max={new Date(2025, 1, 0, 17, 0, 0)}
+            />
     );
 };
 
