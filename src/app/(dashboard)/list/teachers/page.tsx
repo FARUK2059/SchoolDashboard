4c68920a1202bdb38dfde632adee836682f9cd43
FARUK2059 <omarfaruk34552@gmail.com>
2024-09-18 22:36:33 +0600

    pagination design Complet

---
+++ src/app/(dashboard)/list/teachers/page.tsx
@@ -0,0 +1,13 @@
+import Pagination from "@/components/Pagination";
+
+
+
+const page = () => {
+    return (
+        <div>
+            <Pagination></Pagination>
+        </div>
+    );
+};
+
+export default page;
