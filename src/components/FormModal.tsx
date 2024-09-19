f01d77b2a9a59a0fb2c90c2af3e6d8bbacf7eec0
FARUK2059 <omarfaruk34552@gmail.com>
2024-09-20 03:14:42 +0600

    Re Up Modal form

--- src/components/FormModal.tsx
+++ src/components/FormModal.tsx
@@ -1,29 +1,27 @@
-"use client"
+"use client";
 
 import dynamic from "next/dynamic";
 import Image from "next/image";
 import { useState } from "react";
-
 
 // USE LAZY LOADING
 
 // import TeacherForm from "./forms/TeacherForm";
 // import StudentForm from "./forms/StudentForm";
 
-
-// const TeacherForm = dynamic(() => import("./forms/TeacherForm"), {
-//     loading: () => <h1>Loading...</h1>,
-// });
+const TeacherForm = dynamic(() => import("./forms/TeacherForm"), {
+    loading: () => <h1>Loading...</h1>,
+});
 // const StudentForm = dynamic(() => import("./forms/StudentForm"), {
 //     loading: () => <h1>Loading...</h1>,
 // });
 
-// const forms: {
-//     [key: string]: (type: "create" | "update", data?: any) => JSX.Element;
-// } = {
-//     teacher: (type, data) => <TeacherForm type={type} data={data} />,
-//     student: (type, data) => <StudentForm type={type} data={data} />
-// };
+const forms: {
+    [key: string]: (type: "create" | "update", data?: any) => JSX.Element;
+} = {
+    teacher: (type, data) => <TeacherForm type={type} data={data} />,
+    // student: (type, data) => <StudentForm type={type} data={data} />
+};
 
 const FormModal = ({
     table,
@@ -51,15 +49,14 @@ const FormModal = ({
     const size = type === "create" ? "w-8 h-8" : "w-7 h-7";
     const bgColor =
         type === "create"
-            ? "bg-fYellow"
+            ? "bg-lamaYellow"
             : type === "update"
-                ? "bg-fSky"
-                : "bg-fPurple";
+                ? "bg-lamaSky"
+                : "bg-lamaPurple";
 
     const [open, setOpen] = useState(false);
 
-    const FormModal = () => {
-
+    const Form = () => {
         return type === "delete" && id ? (
             <form action="" className="p-4 flex flex-col gap-4">
                 <span className="text-center font-medium">
@@ -69,11 +66,9 @@ const FormModal = ({
                     Delete
                 </button>
             </form>
-        ) 
-        // : type === "create" || type === "update" ? (
-        //     forms[table](type, data)
-        // ) 
-        : (
+        ) : type === "create" || type === "update" ? (
+            forms[table](type, data)
+        ) : (
             "Form not found!"
         );
     };
@@ -89,7 +84,7 @@ const FormModal = ({
             {open && (
                 <div className="w-screen h-screen absolute left-0 top-0 bg-black bg-opacity-60 z-50 flex items-center justify-center">
                     <div className="bg-white p-4 rounded-md relative w-[90%] md:w-[70%] lg:w-[60%] xl:w-[50%] 2xl:w-[40%]">
-                        {/* <Form /> */}
+                        <Form />
                         <div
                             className="absolute top-4 right-4 cursor-pointer"
                             onClick={() => setOpen(false)}
